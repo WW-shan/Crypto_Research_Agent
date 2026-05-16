6e4bf56dc7624b2b9cbf21c255767653d74140a6
WW <212500581@qq.com>
2026-05-16 19:20:34 +0800

    feat: validate close momentum on stored history

--- src/crypto_alpha_agent/validation/__init__.py
+++ src/crypto_alpha_agent/validation/__init__.py
@@ -1,3 +1,4 @@
 from crypto_alpha_agent.validation.market_history import CandleBar, load_candle_history
+from crypto_alpha_agent.validation.momentum import MomentumValidationResult, validate_close_momentum
 
-__all__ = ["CandleBar", "load_candle_history"]
+__all__ = ["CandleBar", "MomentumValidationResult", "load_candle_history", "validate_close_momentum"]
